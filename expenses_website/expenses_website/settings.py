b0c14dd5ec797c1927af9ec4985d3fcd3d6b104a
Youssef <youssefkaramedu@gmail.com>
2024-10-05 22:59:50 +0300

    edit urls in settings

--- expenses_website/expenses_website/settings.py
+++ expenses_website/expenses_website/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expenses'
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'expenses_website/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
