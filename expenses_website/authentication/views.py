5cdb9a012f76438fee77891207b6f83d0da2e215
Youssef <youssefkaramedu@gmail.com>
2024-10-10 21:04:11 +0300

    add function to check if username is alphanumeric

---
+++ expenses_website/authentication/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+import json
+from django.http import JsonResponse
+# Create your views here.
+
+
+class RegistrationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        username=data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error' : 'username should only contain alphanumeric characters'})
+
+class UsernameValidationView(View):
+    def get(self, request):
+        return render(request, 'authentication/register.html')
