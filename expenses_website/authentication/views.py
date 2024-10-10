b9b1bbbec6050af12ec9269b9e60ecbde91ec302
Youssef <youssefkaramedu@gmail.com>
2024-10-10 21:32:54 +0300

    add method to check if username exists in database

--- expenses_website/authentication/views.py
+++ expenses_website/authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 import json
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 
@@ -12,6 +13,10 @@ class UsernameValidationView(View):
 
         if not str(username).isalnum():
             return JsonResponse({'username_error' : 'username should only contain alphanumeric characters'}, status=400)
+        return JsonResponse({'username_valid': True})
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error' : 'username is already used'}, status=409)
         return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
