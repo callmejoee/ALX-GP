bdd5a90346cb685a6cd59f1c3419949650a0e2a3
Youssef <youssefkaramedu@gmail.com>
2024-10-14 00:14:11 +0300

    add home, prices and features to views

--- expenses_website/expenses/views.py
+++ expenses_website/expenses/views.py
@@ -5,5 +5,16 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'expenses/index.html')
 
+def home(request):
+    return render(request, 'partials/home.html')
+
+def prices(request):
+    return render(request, 'partials/prices.html')
+
+
+def features(request):
+    return render(request, 'partials/features.html')
+
+
 def add_expense(request):
     return render(request, 'expenses/add_expenses.html')
