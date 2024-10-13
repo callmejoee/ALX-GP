d0f2b3a530e069e3dadd4601145d568480c3c4b9
Youssef <youssefkaramedu@gmail.com>
2024-10-14 00:12:33 +0300

    add home, pricing, and features to url

--- expenses_website/expenses/urls.py
+++ expenses_website/expenses/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add-expense', views.add_expense, name="add_expenses")
+    path('add-expense', views.add_expense, name="add_expenses"),
+    path('home', views.home, name="home"),
+    path('prices', views.prices, name="prices"),
+    path('features', views.features, name="features")
+
 ]
