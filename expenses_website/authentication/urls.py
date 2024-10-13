69264208382cfa01a4b0ca04593df190296b6aa3
Youssef <youssefkaramedu@gmail.com>
2024-10-14 00:12:13 +0300

    add login page to url

--- expenses_website/authentication/urls.py
+++ expenses_website/authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, VerificationView, LoginView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
@@ -6,5 +6,7 @@ urlpatterns = [
     path('register', RegistrationView.as_view(), name='register'),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name='validate-email'),
-    path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate')
+    path('activate/<uidb64>/<token>', VerificationView.as_view(), name='activate'),
+    path('login', LoginView.as_view(), name='login'),
+
 ]
