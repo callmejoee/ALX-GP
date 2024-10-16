3485f3130debfc64217544882365b33fa8adec41
Youssef <youssefkaramedu@gmail.com>
2024-10-16 15:42:33 +0300

    add login required to the main index page

--- expenses_website/expenses/views.py
+++ expenses_website/expenses/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
 
 # Create your views here.
 
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url="/authentication/login")
 def index(request):
     return render(request, 'expenses/index.html')
 
+
 def home(request):
     return render(request, 'partials/home.html')
+
 
 def prices(request):
     return render(request, 'partials/prices.html')
