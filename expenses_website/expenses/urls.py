10c05b296265073d35a97e2c92cb5f18c1e03b50
Youssef <youssefkaramedu@gmail.com>
2024-10-16 23:41:42 +0300

    add pathes for edit-expense, delete and add another path for index

--- expenses_website/expenses/urls.py
+++ expenses_website/expenses/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add-expense', views.add_expense, name="add_expenses"),
+    path('', views.index, name="expenses"),
+    path('', views.index, name="index"),
+    path('add-expense', views.add_expense, name="add-expense"),
+    path('edit-expense/<int:id>', views.expense_edit, name="expense_edit"),
     path('home', views.home, name="home"),
     path('prices', views.prices, name="prices"),
-    path('features', views.features, name="features")
+    path('features', views.features, name="features"),
+    path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
 
 ]
