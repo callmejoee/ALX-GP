ace7af84fa9d6d1ce0742dc952a718683c3ca2da
Youssef <youssefkaramedu@gmail.com>
2024-10-18 00:04:04 +0300

    implement the summary or the last month stats for income

--- expenses_website/userincome/urls.py
+++ expenses_website/userincome/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('add-income', views.add_income, name="add-income"),
     path('edit-income/<int:id>', views.income_edit, name="income_edit"),
     path('income-delete/<int:id>', views.delete_income, name="income-delete"),
+    path('income_source_sumarry', views.income_source_sumarry, name="income_source_sumarry"),
+    path('income-stats', views.statsView, name="income-stats"),
 
 ]
