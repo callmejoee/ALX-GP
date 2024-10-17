be418e27e188244c2dd4e484b8075cd556df6429
Youssef <youssefkaramedu@gmail.com>
2024-10-17 19:49:28 +0300

    add currency to the views

--- expenses_website/expenses/views.py
+++ expenses_website/expenses/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import cache_control
 from .models import Category, Expense
+from userpreferences.models import UserPreference
 from django.contrib import messages
 from django.core.paginator import Paginator
+import datetime
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -15,14 +18,22 @@ def index(request):
     paginator = Paginator(expense, 5)
     page_number=request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
+    try:
+        currency = UserPreference.objects.get(user=request.user).currency
+    except UserPreference.DoesNotExist:
+        currency = ""
     context = {
         'expenses': expense,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'currency': currency
     }
     categories = Category.objects.all()
     return render(request, 'expenses/index.html', context)
 
 
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url="/authentication/login")
 def add_expense(request):
     if request.method == "POST":
         amount = request.POST['amount']
@@ -34,16 +45,29 @@ def add_expense(request):
         messages.success(request, "Expense saved successfully")
         return redirect("expenses")
 
+    try:
+        currency = UserPreference.objects.get(user=request.user).currency
+    except UserPreference.DoesNotExist:
+        currency = ""
+
     categories = Category.objects.all()
     context = {
-        'categories':categories
+        'categories':categories,
+        'currency': currency,
     }
     return render(request, 'expenses/add_expenses.html', context)
 
 
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url="/authentication/login")
 def expense_edit(request, id):
     expense = Expense.objects.get(pk=id)
     categories = Category.objects.all()
+    try:
+        currency = UserPreference.objects.get(user=request.user).currency
+    except UserPreference.DoesNotExist:
+        currency = ""
 
     if request.method == "POST":
         expense.amount = request.POST.get('amount', expense.amount)
@@ -58,6 +82,7 @@ def expense_edit(request, id):
     context = {
         'expense': expense,
         'categories': categories,
+        'currency': currency,
         'values': {
             'amount': expense.amount,
             'date': expense.date,
@@ -68,6 +93,10 @@ def expense_edit(request, id):
 
     return render(request, 'expenses/edit-expense.html', context)
 
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url="/authentication/login")
 def delete_expense(request, id):
     expense=Expense.objects.get(pk=id)
     expense.delete()
@@ -75,7 +104,37 @@ def delete_expense(request, id):
     return redirect('expenses')
 
 
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@login_required(login_url="/authentication/login")
+def expense_category_sumarry(request):
+    todays_date= datetime.date.today()
+    last_quarter = todays_date - datetime.timedelta(30)
+    expenses=Expense.objects.filter(date__gte=last_quarter, date__lte=todays_date, owner=request.user)
 
+    final_rep = {}
+    
+    def get_category(expense):
+        return expense.category
+
+    category_list = list(set(map(get_category, expenses)))
+
+    def get_expense_category_amount(category):
+        amount = 0
+        filtered_by_category = expenses.filter(category=category)
+
+        for item in filtered_by_category:
+            amount += item.amount
+
+        return amount
+
+    for x in expenses:
+        for y in category_list:
+            final_rep[y] = get_expense_category_amount(y)
+    return JsonResponse({'expense_category_data': final_rep}, safe=False)
+
+
+def statsView(request):
+    return render(request, 'expenses/stats.html')
 
 
 def home(request):
